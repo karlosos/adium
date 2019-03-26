9946044b0ca7ad801388f16cdae4481f199d0538
Karol Dzialowski <karlososhd@gmail.com>
2019-03-26 21:05:26 +0100

    Add variance sum ration to PCA

--- PCA.py
+++ PCA.py
@@ -18,7 +18,7 @@ import matplotlib.gridspec as gridspec
 from sklearn.model_selection import train_test_split
 
 
-def pca(data, components=None):
+def pca(data, components=None, variance_sum_ratio=None):
     """
     Perform Principal Components Analysis
     :param data: source data for PCA
@@ -44,13 +44,26 @@ def pca(data, components=None):
     L = L[ordering]
     V = V[:, ordering]
 
-    if components is not None:
+    if variance_sum_ratio is not None:
+        for i in range(len(L)):
+            if L[:i]/L >= variance_sum_ratio:
+                L, V, _ = variance_sum_ratio(L, V, variance_sum_ratio)
+
+    elif components is not None:
         L = L[:components]
         V = V[:, :components]
 
     elapsed_time = time.time() - start_time
     print("time eig:", elapsed_time)
     return L, V
+
+
+def slice_variance_sum_ratio(L, V, variance_sum_ratio):
+    for i in range(len(L)):
+        if np.sum(L[:i])/np.sum(L) >= variance_sum_ratio:
+            L = L[:i]
+            V = V[:, :i]
+            return L, V, i
 
 
 def pickle_all(some_list, fname):
@@ -109,21 +122,44 @@ def load_pca_or_generate(X_train):
         return L, V
 
 
-def reconstructions(x, V, dims=[10, 20, 30], x_mean=None):
-    n = V.shape[0]
+def reconstructions_components(x, V, dims, x_mean=None):
+    # size of data (size of single image)
+    l = V.shape[0]
     if x_mean is not None:
         x = x - x_mean
-    x_new = np.dot(V[:, 0:np.max(dims)].T, x)
-    reconstr = np.zeros((len(dims), n))
+
+    # l_i = V.T \cdot xi
+    # otrzymujemy wagi
+    x = np.dot(V.T, x)
+
+    # rekonstrukcja
+    # tworzymy listi obrazow rekonstrukcji
+    reconstructions = np.zeros((len(dims), l))
     for i, dim in enumerate(dims):
-        reconstr[i] = V[:, :dim].dot(x_new[:dim])
+        reconstructions[i] = V[:, :dim].dot(x[:dim])
         if x_mean is not None:
-            reconstr[i] += x_mean
+            reconstructions[i] += x_mean
 
-    return reconstr
+    return reconstructions
 
 
-def show_image_pairs(originals, reconstrs, title, titles_2, maes):
+def reconstructions_variance_sum_ratios(x, L, V, variance_sum_ratios, x_mean=None):
+    # size of data (size of single image)
+    l = V.shape[0]
+    if x_mean is not None:
+        x = x - x_mean
+    x = np.dot(V.T, x)
+    reconstructions = np.zeros((len(variance_sum_ratios), l))
+    for i, variance_sum_ratio in enumerate(variance_sum_ratios):
+        _, _, slice_index = slice_variance_sum_ratio(L, V, variance_sum_ratio)
+        reconstructions[i] = V[:, :slice_index].dot(x[:slice_index])
+        if x_mean is not None:
+            reconstructions[i] += x_mean
+
+    return reconstructions
+
+
+def show_image_pairs(originals, reconstructions, main_title, subtitles, maes):
     number_of_pairs = originals.shape[0]
 
     shape = originals[0].shape
@@ -131,13 +167,13 @@ def show_image_pairs(originals, reconstrs, title, titles_2, maes):
         img_side = int(np.sqrt(shape))
         originals = originals.reshape(originals.shape[0], img_side, img_side)
 
-    shape = reconstrs[0].shape
+    shape = reconstructions[0].shape
     if len(shape) == 1:  # flattened images
         img_side = int(np.sqrt(shape))
-        reconstrs = reconstrs.reshape(reconstrs.shape[0], img_side, img_side)
+        reconstructions = reconstructions.reshape(reconstructions.shape[0], img_side, img_side)
 
     fig = plt.figure(figsize=(10, 10))
-    fig.suptitle(title)
+    fig.suptitle(main_title)
 
     grid_size = int(np.ceil(np.sqrt(number_of_pairs)))
     outer = gridspec.GridSpec(grid_size, grid_size, wspace=0.2, hspace=0.2)
@@ -147,7 +183,7 @@ def show_image_pairs(originals, reconstrs, title, titles_2, maes):
                                                  subplot_spec=outer[i], wspace=0.1, hspace=0.1)
 
         ax = plt.Subplot(fig, inner[0])
-        ax.set_title(titles_2[i])
+        ax.set_title(subtitles[i])
         ax.set_xticks([])
         ax.set_yticks([])
         ax.imshow(originals[i])
@@ -157,7 +193,7 @@ def show_image_pairs(originals, reconstrs, title, titles_2, maes):
         bx.set_title(np.around(maes[i], 5))
         bx.set_xticks([])
         bx.set_yticks([])
-        bx.imshow(reconstrs[i])
+        bx.imshow(reconstructions[i])
         fig.add_subplot(bx)
 
     fig.show()
@@ -187,31 +223,27 @@ def main():
     #show_some_images(some_eigen_faces, range(n_eigen_faces), title='First eigen faces')
 
     img = X_test[5, :].T
-    dims = [0, 1, 100, 200, 300, 400, 450, 500, 750, 1000, 2000, 2500, 3000, 3500, 4096]
-    reconstrs = reconstructions(img, V, dims, x_mean=x_mean)
-    originals = np.tile(img, (len(dims), 1))
-    maes = [np.sum(np.abs(originals[i]-reconstrs[i])) / reconstrs[i].size for i in range(len(dims))]
-    plt.plot(dims, maes)
+    plt.imshow(img.reshape(64, 64))
     plt.show()
-    show_image_pairs(originals, reconstrs, 'Rekonstrukcja PCA', titles_2=['dim:' + str(dim) for dim in dims], maes=maes)
 
-    #show_some_images()
+    components = [0, 1, 100, 200, 300, 400, 450, 500, 750, 1000, 2000, 2500, 3000, 3500, 4096]
+    reconstructions = reconstructions_components(img, V, components, x_mean=x_mean)
+    originals = np.tile(img, (len(components), 1))
+    maes = [np.sum(np.abs(originals[i]-reconstructions[i])) / reconstructions[i].size for i in range(len(components))]
+    plt.plot(components, maes)
+    plt.show()
+    show_image_pairs(originals, reconstructions, 'Rekonstrukcja PCA', subtitles=['dim of V:' + str(dim) for dim in components], maes=maes)
 
 
-    # rekonstrukcja obrazow testowych na podstawie twarzy wlasnych
-    # funkcja reconstructions(img, b, dims, x_mean=x_mean)
+    img = X_test[5, :].T
+    variance_sum_ratios = np.asarray([0, 0.1, 0.3, 0.5, 0.6, 0.7, 0.8, 0.9, 0.91, 0.92, 0.93, 0.95, 0.98, 0.99, 0.995, 0.999])
+    reconstructions = reconstructions_variance_sum_ratios(img, L, V, variance_sum_ratios, x_mean=x_mean)
+    originals = np.tile(img, (len(variance_sum_ratios), 1))
+    maes = [np.sum(np.abs(originals[i]-reconstructions[i])) / reconstructions[i].size for i in range(len(variance_sum_ratios))]
+    plt.plot(variance_sum_ratios, maes)
+    plt.show()
+    show_image_pairs(originals, reconstructions, 'Rekonstrukcja PCA', subtitles=['vsr:' + str(vsr) for vsr in variance_sum_ratios], maes=maes)
 
-    # img = X_test[0, :].T
-    # dims = [0, 1, 10, 20, 30, 50, 100..., 4096]
-    # reconstr = reconstructions(img, b, dims, x_mean=x_mean)
-    # originals = np.tile(img, (len(doms,), 1))
-
-    # rekonstrukcje iles razy bedzie mnozyc macierz V * pozycje
-    # po lewej oryginal po prawej rekonstrukcja
-    # 0 skladowych glownych, 10 pierwszych - wyswietlac blad srednia roznice bezwzgledna -
-    # orignal - rekonstrukjca
-    # robi rekonstrukcje i wizualizuje wraz z pomiarem bledow
-    #
 
 if __name__ == '__main__':
     main()
